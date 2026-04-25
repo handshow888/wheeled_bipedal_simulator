55883fd4e31208cc28afc66ee6f25fd59f8b292e
handshow <2890454691@qq.com>
2026-04-25 18:58:49 +0800

    add cal_extrinsic_R.py

---
+++ wheeled_bipedal_description/scripts/cal_extrinsic_R.py
@@ -0,0 +1,8 @@
+from scipy.spatial.transform import Rotation as R
+from math import pi
+
+deg2rad = pi/180.0
+
+# 假设雷达相对于 IMU 绕 Z 轴转了 90 度 (0, 0, 1.57)
+r = R.from_euler('xyz', [0*deg2rad, -15*deg2rad, 0])
+print(r.as_matrix())
